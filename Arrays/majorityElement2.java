1814764bea6e6640da80247baf713d29c5abd263
Rajat Kumar <rajat>
2025-12-08 18:03:21 +0530

    solve N/3 majority element

--- Arrays/majorityElement2.java
+++ Arrays/majorityElement2.java
@@ -1,6 +1,46 @@
 import java.util.*;
 
 class Solution {
+
+	public static List<Integer> majorityElement_mooreVotingAlgorithm(int[] nums) {
+		int element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
+		int count1 = 0, count2 = 0;
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (count1 == 0 && nums[i] != element2) {
+				count1++;
+				element1 = nums[i];
+			} else if (count2 == 0 && nums[i] != element1) {
+				count2++;
+				element2 = nums[i];
+			} else if (nums[i] == element1) {
+				count1++;
+			} else if (nums[i] == element2) {
+				count2++;
+			} else {
+				count1--;
+				count2--;
+			}
+		}
+
+		int element1Count = 0, element2Count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == element1) {
+				element1Count++;
+			} else if (nums[i] == element2) {
+				element2Count++;
+			}
+		}
+		if (element1Count > nums.length / 3) {
+			list.add(element1);
+		}
+		if (element2Count > nums.length / 3) {
+			list.add(element2);
+		}
+		return list;
+	}
+
 	public static List<Integer> majorityElement(int[] nums) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
