9eb3dafc99f7df4b767e41508d11324998ff72f2
Rajat Kumar <rajat>
2025-12-18 19:46:10 +0530

    print max subarray

--- Arrays/maximum_subarray.java
+++ Arrays/maximum_subarray.java
@@ -2,6 +2,7 @@ package Arrays;
 
 class maximum_subarray {
 
+	// return max sum
 	public static int maximumSubarray(int[] arr) {
 		// input : {-1,1,-2,4,1,-3}
 		int currentSum = 0, maxSum = Integer.MIN_VALUE;
@@ -17,8 +18,38 @@ class maximum_subarray {
 		return maxSum;
 	}
 
+	// print the maximum sum subarray
+	public static void printMaximumSubarray(int[] arr) {
+		int currentSum = 0, maxSum = Integer.MIN_VALUE;
+		int start = 0;
+		int ansStart = -1, ansEnd = -1;
+
+		int i = 0;
+		while (i < arr.length) {
+			if (currentSum == 0) {
+				start = i;
+			}
+			currentSum = currentSum + arr[i];
+
+			if (currentSum > maxSum) {
+				ansStart = start;
+				ansEnd = i;
+				maxSum = currentSum;
+			}
+			if (currentSum < 0) {
+				currentSum = 0;
+			}
+			i++;
+		}
+
+		for (int j = ansStart; j <= ansEnd; j++) {
+			System.out.print(arr[j] + ", ");
+		}
+	}
+
 	public static void main(String[] args) {
-		int[] arr = { -1, 1, -2, 4, 1, -3 };
-		System.out.println(maximumSubarray(arr));
+		int[] arr = { -1, 1, -2, 4, 1, -3, 10 };
+		// System.out.println(maximumSubarray(arr));
+		printMaximumSubarray(arr);
 	}
 }
