197be5aeb1fd011b2158dcc057951539a25d1ae9
Rajat Kumar <rajat>
2025-12-27 21:05:39 +0530

    fix iterative atoi

--- Strings/atoi.java
+++ Strings/atoi.java
@@ -13,19 +13,29 @@ class Solution {
 		if (i == s.length()) {
 			return (int) ans;
 		}
+
 		int sign = 1;
-		if (s.charAt(i) == '-') {
-			sign = -1;
-		}
-		i++;
-		while (i < s.length() && Character.isDigit(s.charAt(i))) {
-			ans = ans * 10 + (s.charAt(i) - '0');
+		if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+			sign = s.charAt(i) == '-' ? -1 : 1;
 			i++;
 		}
+
+		while (i < s.length() && Character.isDigit(s.charAt(i))) {
+			ans = ans * 10 + (s.charAt(i) - '0');
+
+			if (sign * ans < Integer.MIN_VALUE) {
+				return Integer.MIN_VALUE;
+			}
+			if (sign * ans > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+			i++;
+		}
+
 		return (int) (ans * sign);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(myAtoi("-00000"));
+		System.out.println(myAtoi("-91283472332"));
 	}
 }
